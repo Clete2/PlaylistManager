3afc5b7f8ef31b21e62376d6fab5d08cf2799dda
Clete Blackwell II <clete2@gmail.com>
2010-12-01 20:55:41 -0500

    Database changes.

--- src/playlistmanager/DatabaseCreator.java
+++ src/playlistmanager/DatabaseCreator.java
@@ -8,21 +8,28 @@ import java.sql.Statement;
 public class DatabaseCreator {
 	private static Connection dbConnection;
 	private static Statement  dbStatement;
-	
+
 	public static void createDatabase() {
-		DatabaseCreator.prepareConnection();
-		String sql = "CREATE TABLE songs (song_id INTEGER PRIMARY KEY," +
+		try {
+			DatabaseCreator.prepareConnection();
+			String query = "CREATE TABLE songs (song_id INTEGER PRIMARY KEY," +
 			" song_title STRING, song_artist STRING, album_id INTEGER," +
 			" song_genre STRING, song_rating INTEGER, absolute_path STRING)";
-		
-		try {
-			dbStatement.executeUpdate(sql);
+			dbStatement.executeUpdate(query);
+			
+			query = "CREATE TABLE album (album_id INTEGER PRIMARY KEY," +
+			" album_name STRING, album_year INTEGER, artist_id INTEGER)";
+			dbStatement.executeUpdate(query);
+			
+			query = "CREATE TABLE artist (artist_id INTEGER PRIMARY KEY," +
+			" artist_first_name STRING, artist_last_name STRING)";
+			dbStatement.executeUpdate(query);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		DatabaseCreator.closeConnection();
 	}
-	
+
 	private static void prepareConnection() {
 		// Load SQLite JDBC driver.
 		try {
@@ -30,7 +37,7 @@ public class DatabaseCreator {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		try {
 			dbConnection = DriverManager.getConnection("jdbc:sqlite:" +
 					PlaylistVariables.getFullDBName());
@@ -40,7 +47,7 @@ public class DatabaseCreator {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void closeConnection() {
 		if(dbConnection != null) {
 			try {
